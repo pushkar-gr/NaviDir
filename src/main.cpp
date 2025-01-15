81f25088ff3c187d67adc84c4c5abc3b9d804fb2
pushkar-gr <gr.pushkar.7@gmail.com>
2025-01-15 20:12:29 +0530

    add: take path as an argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include "backend/FileManager.hpp"
 #include "frontend/UI.hpp"
 #include "config/Config.hpp"
+#include <filesystem>
 
-int main() {
+int main(int size, char **args) {
   Config config;
-  FileManager fm(&config);
-  UI ui(&fm, &config);
+  FileManager *fm;
+  if (size == 2) {
+    path path = args[1];
+    if (exists(path)) {
+      fm = new FileManager(path, &config);
+    } else {
+      fm = new FileManager(&config);
+    }
+  } else {
+    fm = new FileManager(&config);
+  }
+  UI ui(fm, &config);
   return 0;
 }
